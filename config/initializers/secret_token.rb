45da80843ba806cecc1dcb1e122d348de4bc6b36
Joaquin Gamboa <gamboanj@gmail.com>
2013-06-08 20:56:20 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PickUpSports::Application.config.secret_token = 'c242d0dded0beffba8d485e7f5b77311b528441fb3d23b88b6a1a5b6af91dc1705e54b562711d9c54027363d7d61523d2698f267a461cd74b05aa119ed82576f'
