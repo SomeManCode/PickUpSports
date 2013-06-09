9afb78ccf3711a9a73651ced165feb49bebcd4df
Joaquin Gamboa <gamboanj@gmail.com>
2013-06-09 15:51:36 -0400

    Setting up geocoder

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,3 +1,10 @@
 class Sport < ActiveRecord::Base
   attr_accessible :address, :game, :latitude, :longitude, :needed, :zipcode, :state
+
+  geocoded_by :geolocate
+
+  def geolocate
+     "#{self.address}"', '"#{self.state}"', '"#{self.zipcode}"
+  end
+
 end
