1d763c09c10392843c3b4b11901d83bc4d711aec
Joaquin Gamboa <gamboanj@gmail.com>
2013-06-10 14:23:56 -0400

    Added relationships between Users and Sports models

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,8 +1,10 @@
 class Sport < ActiveRecord::Base
-  attr_accessible :address, :game, :latitude, :longitude, :needed, :zipcode, :state
+  attr_accessible :address, :game, :latitude, :longitude, :needed, :zipcode, :state, :user_id
 
   geocoded_by :geolocate
   after_validation :geocode
+
+  belongs_to :user
 
   def geolocate
      "#{self.address}"', '"#{self.state}"', '"#{self.zipcode}"
