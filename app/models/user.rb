3d9278ef90da9e6204b7df969fbd81e79a151ee8
Joaquin Gamboa <gamboanj@gmail.com>
2013-06-10 14:16:04 -0400

    Created and displayed username field for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :avatar, :username
   # attr_accessible :title, :body
 
   mount_uploader :avatar, AvatarUploader
